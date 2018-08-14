50699a2944f64e54ada24f5fea70ab5b62a78a90
jlgm <jlgm@cin.ufpe.br>
2018-08-14 18:15:58 -0300

    add tests for robots movement

--- test/dinos/core_test.clj
+++ test/dinos/core_test.clj
@@ -75,28 +75,77 @@
 ;; testing robots commands
 
 (deftest move-test
-  (testing "TODO"
-    (is (= 1 1))))
+  (testing "if function returns the sum of given coordinates with a direction vector"
+    (is (= (move [2 2] [-1 0]) [1 2]))
+    (is (= (move [0 0] [1 0]) [1 0]))
+    (is (= (move [2 2] [0 1]) [2 3] ))))
+
+(deftest can-move?-test
+  (let [a-board (new-board 10)]
+    (testing "if move is still inside the board"
+      (is (can-move? a-board [2 2]))
+      (is (can-move? a-board [9 9])))
+    (testing "if move is outside the range of the board"
+      (is (not (can-move? a-board [10 10])))
+      (is (not (can-move? a-board [-1 2]))))))
+
+(deftest do-move-test
+  (let [a-board (place-robot (new-board 10) [0 0])
+        a-dino-board (place-dino a-board [1 0])]
+    (testing "if robot by default moves 'to the right'"
+      (is (= (do-move a-board [0 0] identity) (place-robot (new-board 10) [1 0]))))
+    (testing "if robot can't visit a place with a dino on it"
+      (is (= (do-move a-dino-board [0 0] identity) a-dino-board)))))
+
+(deftest fwd-move-test
+  (let [a-board (place-robot (new-board 10) [2 2])]
+    (testing "if default robot walks for its 'right' on a fwd-move"
+      (is (= (fwd-move a-board [2 2]) (place-robot (new-board 10) [3 2]))))))
 
 
+(deftest rev-move-test
+  (let [a-board (place-robot (new-board 10) [2 2])]
+    (testing "if default robot walks for its 'left' on a rev-move"
+      (is (= (rev-move a-board [2 2]) (place-robot (new-board 10) [1 2]))))))
+
+(deftest turn-right-test
+  (testing "if direction vector turns to the right"
+    (is (= (turn-right [1 0]) [0 1]))
+    (is (= (turn-right [0 1]) [-1 0]))
+    (is (= (turn-right [-1 0]) [0 -1]))
+    (is (= (turn-right [0 -1]) [1 0]))))
+
+(deftest turn-left-test
+  (testing "if direction vector turns to the left"
+    (is (= (turn-left [1 0]) [0 -1]))
+    (is (= (turn-left [0 -1]) [-1 0]))
+    (is (= (turn-left [-1 0]) [0 1]))
+    (is (= (turn-left [0 1]) [1 0]))))
+
+(deftest rotate-right-test
+  (testing "if robots change it's direction vector for a right rotation"
+    (let [a-board (place-robot (new-board 10) [2 2])]
+      (is (= (get-item (rotate-right a-board [2 2]) [2 2]) [0 1])))))
 
 
+(deftest rotate-left-test
+  (testing "if robots change it's direction vector for a left rotation"
+    (let [a-board (place-robot (new-board 10) [2 2])]
+      (is (= (get-item (rotate-left a-board [2 2]) [2 2]) [0 -1])))))
+
+(deftest do-attack-test
+  (testing "if dinos are erased from given position"
+    (let [a-board (place-dino (new-board 10) [1 2])]
+      (is (= (new-board 10) (do-attack a-board [1 2]))))))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(deftest attack-test
+  (testing "if robot erases all dinos from (only) its adjacent cells"
+    (let [a-board (place-robot (new-board 10) [2 2])
+          a-dino-board (place-dino a-board [1 2])
+          an-away-dino-board (place-dino a-board [5 5])]
+      (is (= (attack an-away-dino-board [2 2]) an-away-dino-board))
+      (is (= (attack a-dino-board [2 2]) a-board)))))
 
 
 
